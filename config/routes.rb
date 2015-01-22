c72780ce3b6fd23cb4676203a2cc8fc1f0d797b9
Nina Lisitsa <Ninajl1@Ninas-MacBook-Pro.local>
2015-01-21 19:02:49 -0700

    Delivered FAQ stories page

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,16 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'welcome#index'
+
+  root 'welcome#index'
+
    get 'faq'=>'faq#index'
+
+   get 'about'=>'about#index'
+
+   get 'terms' => 'terms#index'
+
+   get 'tasks' => 'tasks#index'
 
   # Example of regular route:
     get 'products' => 'catalog#view'
