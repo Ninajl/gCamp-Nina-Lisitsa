57e90f514394b03b848e134f849fe29232a31d13
Nina Lisitsa <Ninajl1@Ninas-MacBook-Pro.local>
2015-01-27 18:26:56 -0700

    Add due date colum to index page and line on show page and completed checkbox

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150126205951) do
+ActiveRecord::Schema.define(version: 20150128004056) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
     t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.date     "task_due_date"
+    t.boolean  "completed"
   end
 
 end
