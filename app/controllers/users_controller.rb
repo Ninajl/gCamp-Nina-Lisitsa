b710323105a72640073b6d30f3e02c3936fde087
Nina Lisitsa <Ninajl1@Ninas-MacBook-Pro.local>
2015-02-04 16:24:08 -0700

    Add delete button and alert to my user edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,6 +34,13 @@ class UsersController <ApplicationController
         end
       end
 
+      def destroy
+        @user = User.find(params[:id])
+        if @user.destroy
+          redirect_to users_url, alert: 'User was successfully destroyed'
+        end
+      end
+
     private
     def user_params
       params.require(:user).permit(:first_name, :last_name, :email)
