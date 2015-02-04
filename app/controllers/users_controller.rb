822d9656b67a4c6ca5453f3850c18e11b0b74340
Nina Lisitsa <Ninajl1@Ninas-MacBook-Pro.local>
2015-02-04 16:01:26 -0700

    Add user edit button and page to gCamp

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,9 +19,23 @@ class UsersController <ApplicationController
           render :new
         end
       end
-    end
+
+      def edit
+        @user = User.find(params[:id])
+      end
+
+      def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+
+          redirect_to users_path
+        else
+          render :edit
+        end
+      end
 
     private
     def user_params
       params.require(:user).permit(:first_name, :last_name, :email)
     end
+end
