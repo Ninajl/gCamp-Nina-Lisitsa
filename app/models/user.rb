9788bf1b77f640acd4a1315d4bb7c5a1aa36dd66
Nina Lisitsa <Ninajl1@Ninas-MacBook-Pro.local>
2015-02-26 13:33:16 -0700

    fix user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
-  validates :first_name, :last_name, :password, presence: true
+  validates :first_name, :last_name, presence: true
   validates :email, presence: true, uniqueness: true
 
   def fullname
