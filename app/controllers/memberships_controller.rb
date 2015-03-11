72f525d45486e69340841f9227d09a8eac9fb387
nina <ninajl@gmail.com>
2015-03-10 19:44:33 -0600

    Link to projects memberships index appears on the project show page

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,5 +1,20 @@
 class MembershipsController < ApplicationController
 
+  def index
+    @project = Project.find(params[:project_id])
+    @memberships = @project.memberships.all
+    # @membership = Membership.find(params[:id])
 
+  end
+
+  def create
+    @membership = Membership.new(membership_params)
+
+  end
+
+  private
+  def membership_params
+    params.require(:membership).permit(:project_id, :user_id, :role)
+  end
 
 end
