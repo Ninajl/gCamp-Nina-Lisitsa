b302dd62244286467bf64721fd18bfa9283c0e90
nina <ninajl@gmail.com>
2015-04-10 15:46:35 -0600

    Add capybara coverage to signup

--- spec/features/user_auth_sign_up.rb
+++ spec/features/user_auth_sign_up.rb
@@ -4,11 +4,11 @@ describe 'Registering a Guest' do
 
   scenario 'Guest must register with valid data' do
     visit '/signup'
-    fill_in 'first_name', :with => "Nina"
-    fill_in 'last_name', :with => "Lisitsa"
-    fill_in 'email', :with => "nina@go.com"
-    fill_in 'password', :with => "test11"
-    fill_in 'password_confirmation', :with => "test11"
+    fill_in 'First name', :with => "Nina"
+    fill_in 'Last name', :with => "Lisitsa"
+    fill_in 'Email', :with => "nina@go.com"
+    fill_in 'Password', :with => "test11"
+    fill_in 'Password confirmation', :with => "test11"
     click_button 'Sign Up'
     expect(page).to have_content "Sign into gCamp!"
 
