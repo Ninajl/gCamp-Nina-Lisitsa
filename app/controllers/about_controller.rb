440499415ce37deefec6c73db03d6d571b23eecd
nina <ninajl@gmail.com>
2015-04-15 08:45:56 -0600

    Users should see the total counts of projects, memberships, tasks and comments on the about page

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,6 +1,11 @@
 class AboutController <ApplicationController
   before_action :authenticate
-  
+
   def index
+    @projects = Project.all
+    @tasks = Task.all
+    @users =  User.all
+    @comments = Comment.all
+    @memberships = Membership.all
   end
 end
